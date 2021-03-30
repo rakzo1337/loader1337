6504dc2aacc42d53ff02aba5b41f37daea4003a5
rakzo1337 <76841968+rakzo1337@users.noreply.github.com>
2021-03-30 20:13:28 +0300

    Update check.php

--- check.php
+++ check.php
@@ -1,3 +1,3 @@
-username = rakzo
-password = maks123123
-hwid = 5C59-E15F-4607-EC34-B1DA-A1BD-1C3C-879E
+$username = $_GET['rakzo'];
+$password = $_GET['maks123123'];
+$hwid = $_GET['5C59-E15F-4607-EC34-B1DA-A1BD-1C3C-879E'];
